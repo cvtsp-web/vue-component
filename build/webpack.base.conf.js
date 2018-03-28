21a940630d5dcbe3c87247b13910799c248b2641
wukangjun <838927844@qq.com>
2018-03-28 17:01:31 +0800

    modify test page

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -13,7 +13,7 @@ function resolve (dir) {
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: './example/main.js'
+    app: './test/main.js'
   },
   output: {
     path: config.build.assetsRoot,
